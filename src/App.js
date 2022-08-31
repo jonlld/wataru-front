7811314732d5d84674a661e5f804be904aa98dfe
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-31 20:42:30 +0900

    add test input and log data retrieved prior to calling API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import Header from "./components/Header";
+import Phrases from "./components/Phrases";
 
 const App = () => {
-  return <h1>Konnichiwa</h1>;
+  const [phrases, setPhrases] = useState([]);
+  const requestRef = useRef();
+
+  // fetch request
+  const requestHandler = async (e) => {
+    e.preventDefault();
+
+    // read and format data
+    const enteredRequest = requestRef.current.value;
+    const formattedRequest = {
+      request_lang: "ja",
+      request_string: enteredRequest,
+    };
+
+    const url = "http://localhost/8000";
+    const options = {
+      method: "POST",
+      body: JSON.stringify(formattedRequest),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    console.log(formattedRequest);
+
+    // const res = await fetch(`${url}/api/phrases`, options);
+    // const data = await res.json();
+
+    // console.log(data);
+  };
+
+  return (
+    <div>
+      <Header />
+      <form onSubmit={requestHandler}>
+        <label htmlFor="phrase">Phrase</label>
+        <input type="text" id="phrase" ref={requestRef}></input>
+        <button type="submit">Submit</button>
+      </form>
+      <Phrases phrases={phrases} />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Phrases.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Phrases = (props) => {
+  return <p>Test Entry</p>;
+};
+
+export default Phrases;
